ff606517ee8f1724badfedd59340b582a8a3561f
chihuo2104 <chihuo2104@moekonnyaku.com>
2022-08-06 15:34:36 +0800

    chore: hid cn tip

--- src/views/ArticleList.vue
+++ src/views/ArticleList.vue
@@ -4,7 +4,7 @@ import mockposts from '../mocks/posts'
 import mocksettings from '@/mocks/settings'
 
 import { useStore } from 'vuex'
-import { watch, getCurrentInstance, ref } from 'vue'
+import { ref } from 'vue'
 import { marked } from 'marked'
 import { useRouter } from 'vue-router'
 
@@ -12,7 +12,7 @@ import { useRouter } from 'vue-router'
 
 const $router = useRouter()
 const $store = useStore()
-const instance = getCurrentInstance()
+// const instance = getCurrentInstance()
 
 // gh callback
 // Keep clean!
@@ -33,7 +33,7 @@ if (sessionStorage.getItem('code') != null && sessionStorage.getItem('code') !==
 }
 
 // watch
-const isCN = watch(() => $store.state.isCN, () => { instance.proxy.$forceUpdate() })
+// const isCN = watch(() => $store.state.isCN, () => { instance.proxy.$forceUpdate() })
 
 const posts = ref({})
 const settings = ref({})
@@ -54,26 +54,26 @@ posts.value.sort((a, b) => {
 })
 console.log(posts.value)
 // methods
-function ifcn (china) {
-  if ($store.state.isCN === true) {
-    if (china === true) { // if in China? and post support china
-      return true
-    } else {
-      return false
-    }
-  } else { // abroad
-    return true
-  }
-}
+// function ifcn (china) {
+//   if ($store.state.isCN === true) {
+//     if (china === true) { // if in China? and post support china
+//       return true
+//     } else {
+//       return false
+//     }
+//   } else { // abroad
+//     return true
+//   }
+// }
 </script>
 <template>
   <div id="article-list">
     <h2>文章列表</h2>
     <div v-html="marked.parse(settings.site.announcement)" id="announcement"></div>
-    <h2 v-if="isCN">由于您目前位于中国大陆地区，为符合中国大陆的法律法规，已将部分内容进行隐藏。<br/><small>如果您已经确定您正在使用非中国大陆IP访问，请刷新页面并等待5-10秒......</small></h2>
+    <!-- <h2 v-if="isCN">由于您目前位于中国大陆地区，为符合中国大陆的法律法规，已将部分内容进行隐藏。<br/><small>如果您已经确定您正在使用非中国大陆IP访问，请刷新页面并等待5-10秒......</small></h2> -->
     <ol>
       <div v-for="i in posts" :key="i.id">
-        <div class="article" v-if="ifcn(i.china)"><li><router-link :to="'/posts/' + i.path">{{i.title}}</router-link></li></div>
+        <div class="article"><li><router-link :to="'/posts/' + i.path">{{i.title}}</router-link></li></div>
       </div>
     </ol>
   </div>
