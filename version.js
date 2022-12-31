762598b540cacf8b5645bc822353cd113318d3ed
chihuo2104 <i@chihuo2104.dev>
2022-12-31 10:35:35 +0800

    chore: prepare for release 1.1

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 export default {
-  version: '1.0.9',
-  versionReleaseDate: '20221221',
-  versionDisplay: '1.0.9(20221221)',
-  supportVersionDate: '20220924',
-  supportVersion: '1.0.7'
+  version: '1.1.0',
+  versionReleaseDate: '20221231',
+  versionDisplay: '1.1.0(20221231)',
+  supportVersionDate: '20221231',
+  supportVersion: '1.1.0'
 }
