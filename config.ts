2624931bc4152e9722e35d5d25482783f8267609
chihuo2104 <i@chihuo2104.dev>
2023-02-25 10:27:27 +0800

    chore: prepared to release 1.1.2

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export default {
-  model: 'mocks',
-  settings: 'http://127.0.0.1:3000/mock.1.0.8.json'
+  model: 'production', // mocks is the mockdata in mocks folder; production uses the mock2get json file.
+  settings: 'http://127.0.0.1:3000/mock.1.1.2.json'
 }
