2e57cefe55462509fa46d06ed7b90191298633ff
chihuo2104 <i@chihuo2104.dev>
2023-05-01 13:22:52 +0800

    chore: polyfill & nexttick

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,6 +7,11 @@ const props = defineProps({
   s: String,
   textCount: String
 })
+import { nextTick, ref } from 'vue'
+const tickTime = ref(new Date())
+nextTick(() => {
+  tickTime.value = new Date()
+})
 const settings = props.settings
 </script>
 <template>
@@ -15,7 +20,7 @@ const settings = props.settings
     <div id="footer">
       <div id="cpr" v-show="settings.site.showcopyright">本页面由<a :href="settings.site.author.url" target="_blank">{{settings.site.author.name}}</a>进行维护。版权所有&copy;{{settings.site.copyright.startyear}}-{{ new Date().getFullYear() }}。</div>
       <div>
-        <span v-show="settings.site.showstats">页面生成于{{ s }}，最后渲染于{{(new Date()).toLocaleString()}}。加载&nbsp;{{ loadTime }}ms&nbsp;渲染&nbsp;{{ renderTime }}ms</span>
+        <span v-show="settings.site.showstats">页面生成于{{tickTime.toLocaleString()}}，最后渲染于{{ s }}。加载&nbsp;{{ loadTime }}ms&nbsp;渲染&nbsp;{{ renderTime }}ms</span>
         <span v-show="settings.site.textcount.global">&nbsp;站点总字数：{{ textCount }}</span> <br/>
         <span v-show="settings.site.count.enabled && settings.site.count.site">本站总访问次数：<span id="busuanzi_value_site_pv">加载中...</span>&nbsp;|&nbsp;本站访客数：<span id="busuanzi_value_site_uv">加载中...</span></span>    
       </div>
