6e346a59f28e4c474005cb099e0a4c80a53d8de9
chihuo2104 <i@chihuo2104.dev>
2023-07-03 23:00:40 +0800

    feat: universal 404 route

---
+++ pages/[...slug].vue
@@ -0,0 +1,38 @@
+<script setup>
+import mocksettings from '@/mocks/settings'
+// import { ref } from 'vue'
+// import { useStore } from '@/store'
+
+const settings = ref({})
+// const $store = useStore()
+const $store = useAlldata()
+
+if ($store.value.model === 'production') {
+  settings.value = $store.value.all.settings
+  useHead({
+    title: '你好像迷路了呢 - ' + settings.value.site.title
+  })
+} else {
+  settings.value = mocksettings
+}
+
+</script>
+<template>
+  <div>
+    <img src="@/assets/notfound.png" title="七七-不在"/>
+    <h2>啊哦，看起来你好像迷路了呢......</h2>
+    <h2><nuxt-link to="/">点我返回</nuxt-link></h2>
+  </div>
+</template>
+<style scoped>
+a:link,a:visited {
+  color: black;
+  text-decoration: none;
+}
+a:hover,a:active {
+  color: cyan;
+}
+</style>
+<style>
+
+</style>
