f61f04fcfaff9a16a5dcb92c0b480b0ec4f19bd1
chihuo2104 <i@chihuo2104.dev>
2023-11-11 13:41:52 +0800

    feat: waline comment system

---
+++ components/CommentSys/Waline.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({
+  backend: String,
+  path: String
+})
+import { Waline } from '@waline/client/component';
+import '@waline/client/dist/waline.css';
+</script>
+<template>
+  <Waline :serverURL="props.backend" :path="path" />
+</template>
