2475c0635bddd58a14e26d84e4e6590c745fa6e6
chihuo2104 <i@chihuo2104.dev>
2023-11-11 13:49:14 +0800

    chore: prepare to release 1.2.1

--- version.ts
+++ version.ts
@@ -4,6 +4,6 @@ export default {
   versionDisplay: '1.2.1(20231111)',
   supportVersionDate: '20230807',
   supportVersion: '1.1.9',
-  type: 'dev',
-  prelease: true
+  type: 'stable',
+  prelease: false
 }
