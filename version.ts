f6ce97d00907d6e6a8660ebeccc71d97e8f7500e
chihuo2104 <i@chihuo2104.dev>
2024-05-02 14:49:38 +0800

    chore: prepared to release 1.2.3

--- version.ts
+++ version.ts
@@ -1,9 +1,9 @@
 export default {
   version: '1.2.3',
-  versionReleaseDate: '[pending]',
+  versionReleaseDate: '20240502',
   versionDisplay: '1.2.2(20240217)',
   supportVersionDate: '20240217',
   supportVersion: '1.2.2',
-  type: 'dev',
-  prelease: true
+  type: 'stable',
+  prelease: false
 }
